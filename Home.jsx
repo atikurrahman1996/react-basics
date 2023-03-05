5f39955f290ab87b16d3a174bf5c0847f4ca847e
ATIKUR RAHMAN <76957521+atikurrahman1996@users.noreply.github.com>
2023-03-05 18:14:49 +0600

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Header />
+      <h1> This is home page</h1>
+    </div>
+  );
+}
+
+export default Home;
